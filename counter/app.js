e16c1f6c38d35971072ba7397e78f5eaa5b99749
Thynes222 <27tristanhynes@sau11.org>
2026-02-09 10:31:41 -0500

    finisihcount

--- counter/app.js
+++ counter/app.js
@@ -11,7 +11,21 @@ btns.forEach(function (btn) {
         if (styles.contains('decrease')){
             count--;
         }
-        else if(styles.contains('increase'))
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = 'green';
+        } 
+        if(count < 0){
+            value.style.color = 'red';
+        } 
+        if(count == 0){
+            value.style.color = 'black';
+        } 
         value.textContent = count;
     });
 });
